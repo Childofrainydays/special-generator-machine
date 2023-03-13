aa1a0327d72ab531370895ae223483c8fbcbf282
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-03-13 12:42:16 -0600

    Commented code for clarity

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+// generatePassword funtion 
+function generatePassword() {
+
+}
+
+//  Character functions
+
 // Write password to the #password input
 function writePassword() {
+  // Store value from generatePassword in var password 
   var password = generatePassword();
+  // Display password on page at HTML ID password
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
