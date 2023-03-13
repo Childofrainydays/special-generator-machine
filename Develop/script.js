f355630d7235af8199b72977a42576b4a1e4be1e
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-03-13 15:12:31 -0600

    Created arrays of character types (uppercase, lowercase, numbers and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Add arrays for character type inclusion options
+    // 1) Uppercase
+    const uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",]
+    // 2) Lowercase
+    const lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
+    // 3) Numbers
+    const numbersArray = [1,2,3,4,5,6,7,8,9,0]
+    // 4) Special characters (EX: !,#,@)
+    const specialArray = ["!","#","$","%","&","*","@"] 
 
 
 // generatePassword funtion 
@@ -11,12 +19,17 @@ function generatePassword() {
 
   //Prompt user for password length input (8-128 character length)
 
+  //Prompt user for password character type inclusion options of:
+    // 1) Uppercase
+    // 2) Lowercase
+    // 3) Numbers
+    // 4) Special characters (EX: !,#,@) 
 
+  // Combine randomized characters from user selected options
 
   //Add return to output generated password value
+  return "Quack";
 }
-
-//  Character functions
 
 // Write password to the #password input
 function writePassword() {
