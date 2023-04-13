222c06daaaa43056df405ae5b9bb89940c5dad12
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-04-12 19:49:15 -0600

    fixed 'return' output and randomized character combination for loop

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,17 @@ var generateBtn = document.querySelector("#generate");
 
     //Alt method possible using String.fromChar()
 
-//Define a function to shuffle an array - do so OUTSIDE and BEFORE the generatePassword function so it can be called later in the generatePassword function
+//Define a function to shuffle an array - do so OUTSIDE and BEFORE the generatePassword function so it can be called later in the generatePassword function.
+//There's so much information about the Fisher-Yates/Knuth/Durstenfeld Shuffle algorithm I figured I should give it a try, so :
+function shuffleArray(characterPool, length) {
+  for (let i = length - 1; i > 0; i--) {
+    // Friendly neighborhood Math.random
+    const j = Math.floor(Math.random() * (i + 1));
+    // To swap elements. Allows you to swap two values without using a temporary variable.
+    [characterPool[i], characterPool[j]] = [characterPool[j], characterPool[i]];
+  }
+  return characterPool;
+}
 
 
 // generatePassword funtion 
@@ -24,19 +34,15 @@ function generatePassword(){
   //Prompt user for password length input (8-128 character length) & change into an integer
   let lengthOf = parseInt(prompt("How long would you like your password to be?"));
 
-   //Prompt user for password character type inclusion options of :
+  //Prompt user for password character type inclusion options of :
+  // 1) Uppercase
   let useUppercase = confirm("Do you want to include uppercase letters?");
+  // 2) Lowercase
   let useLowercase = confirm("Do you want to include lowercase letters?");
+  // 3) Numbers
   let useNumbers = confirm("Do you want to include numbers?");
+  // 4) Special characters (EX: !,#,@)
   let useSpecial = confirm("Do you want to include special characters?");
-
-
-  //rework
-  let options = (special)
-  options.forEach((option) => {
-    confirm("do you want to include" + option)
-  }) 
-  console.log(options) 
   
   const characterPool = [
     // 1) Uppercase
@@ -52,13 +58,19 @@ function generatePassword(){
   //Account for idiots 
   if(characterPool.length === 0) return "invalid input"
 
+  //Make sure there's an empty array to hold the characters
+  let passwordArray = [];
+
   // Combine randomized characters from user selected options using Math.random through the length of the array containing all possible character options
   for(let i = 0; i <lengthOf; i++){  
-    Math.floor(Math.random() * characterPool.length)
+    passwordArray.push(characterPool[Math.floor(Math.random() * characterPool.length)]);
   }
 
+  //Shuffle the array of characters
+  shuffleArray(passwordArray);
+
   //Add return to output generated password value
-  return 
+  return passwordArray.join("");
 }
 
 // Write password to the #password input
